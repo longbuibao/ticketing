8ab64122c145305db433ac3d584b6120c6fbc951
bui bao long <blong1102@gmail.com>
2022-09-07 19:52:04 +0700

    move logic to error object

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,7 +1,11 @@
 export class DatabaseConnectionError extends Error {
   reason = 'Something wrong in the server';
+  statusCode = 500;
   constructor() {
     super();
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+  seriazlizeErrors() {
+    return [{ message: this.reason }];
   }
 }
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -3,23 +3,6 @@ import { Response, Request, NextFunction } from 'express';
 import { DatabaseConnectionError, RequestValidationError } from '../errors';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof RequestValidationError) {
-    const formattedErrors = err.errors.map((error) => {
-      return {
-        message: error.msg,
-        field: error.param,
-      };
-    });
-
-    return res.status(400).send({ errors: formattedErrors });
-  }
-  if (err instanceof DatabaseConnectionError) {
-    return res.status(500).send({
-      errors: [
-        {
-          message: err.reason,
-        },
-      ],
-    });
-  }
+  if (err instanceof RequestValidationError) return res.status(err.statusCode).send({ errors: err.seriazlizeErrors() });
+  if (err instanceof DatabaseConnectionError) return res.status(err.statusCode).send({ errors: err.seriazlizeErrors() });
 };

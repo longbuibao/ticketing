89dc3e7f130f6e7253043d460b9d6837bbb4750d
bui bao long <blong1102@gmail.com>
2022-09-10 10:24:50 +0700

    add app.js file

---
+++ auth/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter, signinRouter, signoutRouter, signupRouter } from './routes';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors';
+
+const app = express();
+app.set('trust proxy', true);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', () => {
+  throw new NotFoundError('Not found this route');
+});
+
+app.use(errorHandler);
+
+export default app;
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,7 @@
-import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter, signinRouter, signoutRouter, signupRouter } from './routes';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors';
-
-const app = express();
-app.set('trust proxy', true);
+import app from './app';
 
 const port = 3000;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', () => {
-  throw new NotFoundError('Not found this route');
-});
-
-app.use(errorHandler);
 
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');

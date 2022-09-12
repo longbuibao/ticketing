24e59d0397770561f2feacdae8bcd88befff2b08
bui bao long <blong1102@gmail.com>
2022-09-12 09:46:39 +0700

    init ticket service with infra

--- ticket/src/index.ts
+++ ticket/src/index.ts
@@ -7,7 +7,7 @@ const start = async () => {
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
   try {
     console.log('Connecting to mongodb...');
-    await mongoose.connect('mongodb://auth-mongo-clusterip-srv:27017/auth');
+    await mongoose.connect('mongodb://ticket-mongo-clusterip-srv:27017/auth');
     console.log('Connected to Mongodb');
   } catch (error) {
     console.error(error);

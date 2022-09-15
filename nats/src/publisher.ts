0d6d058be251dd231e53620e3d072d61ddf8ed7f
bui bao long <blong1102@gmail.com>
2022-09-15 15:09:32 +0700

    publisher listener

---
+++ nats/src/publisher.ts
@@ -0,0 +1,21 @@
+import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
+
+const stan = nats.connect('ticketing', 'abc', {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+  // console.log('Publisher connected to nats');
+  // const data = JSON.stringify({
+  //   title: 'fddfdfdfdf',
+  //   price: 23333,
+  // });
+  // stan.publish('ticket:created', data);
+  const publisher = new TicketCreatedPublisher(stan);
+  publisher.publish({
+    id: 'dlfjdlkf',
+    price: 1,
+    title: 'ldfdfk',
+  });
+});

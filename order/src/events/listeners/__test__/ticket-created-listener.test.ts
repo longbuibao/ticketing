3650c3775804acd39d9e0037fe701a2e1bff5440
bui bao long <blong1102@gmail.com>
2022-09-20 11:16:44 +0700

    add test for created ticket listener

---
+++ order/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,35 @@
+import { TicketCreatedEvent } from "@lbbticket/common";
+import mongoose from "mongoose";
+
+import { TicketCreatedListener } from "../ticket-created-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../../models";
+
+const setup = async () => {
+  const listener = new TicketCreatedListener(natsWrapper.client);
+  const data: TicketCreatedEvent["data"] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    title: "NASA",
+    price: 10,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  };
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg };
+};
+
+it("Create and save a ticket", async () => {
+  const { listener, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+  const ticket = await Ticket.findById(data.id);
+  expect(ticket).toBeDefined();
+  expect(ticket!.version).toEqual(0);
+  expect(ticket!.id).toEqual(data.id);
+});
+
+it("Ack the message", async () => {});

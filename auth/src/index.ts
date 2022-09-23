5595356f4a5bb84d486492398ee857c3f317621c
bui bao long <blong1102@gmail.com>
2022-09-23 11:21:17 +0700

    big feature for auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,7 @@ const port = 3000;
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
   if (!process.env.MONGO_URI) throw new Error('MONGO_URI must be defined');
+  console.log('CHANGE IN AUTH SERVICE');
   try {
     console.log('Connecting to mongodb...');
     await mongoose.connect(process.env.MONGO_URI);

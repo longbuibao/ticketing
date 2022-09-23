0505769793a185ca3231d934d959cdd0d1683c81
bui bao long <blong1102@gmail.com>
2022-09-23 10:04:59 +0700

    add github action, mystery error while exiting mongodb memory server

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,15 +1,11 @@
-import express from "express";
+import express from 'express';
 
 // import { currentUser, requireAuth } from '../middlewares';
-import {
-  currentUser,
-  NotAuthorizedError,
-  requireAuth,
-} from "@lbbticket/common";
+import { currentUser, NotAuthorizedError } from '@lbbticket/common';
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", currentUser, (req, res) => {
+router.get('/api/users/currentuser', currentUser, (req, res) => {
   if (!req.currentUser) throw new NotAuthorizedError();
   res.send({ currentUser: req.currentUser });
 });
